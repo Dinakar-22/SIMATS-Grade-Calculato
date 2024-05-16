5f7daf1849e52bd32a4dc77cd0bb921a972beab2
Dinakar <168794542+Dinakar-22@users.noreply.github.com>
2024-05-16 17:02:36 +0530

    Update 2_©️_Credit.py

--- pages/2_©️_Credit.py
+++ pages/2_©️_Credit.py
@@ -8,6 +8,14 @@ import streamlit as st
 textColor="#FCF7FF"
 font="Press Start 2P"    
 
+st.markdown(f"""
+            <style>
+            .stApp {{background-image: url("https://images.unsplash.com/photo-1523821741446-edb2b68bb7a0?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"); 
+                     background-attachment: fixed;
+                     background-size: cover}}
+         </style>
+         """, unsafe_allow_html=True)
+
 st.set_page_config(page_title="Credits", page_icon="©️")
 
 st.subheader('', divider='rainbow')
